22dffaf01fd7431dbd2360579abdb91ea7b35999
calumacutt <calumacutt@gmail.com>
2021-06-07 23:30:29 +1000

    Trying to make cpp q1 more efficient

--- cpp/q1/main.cpp
+++ cpp/q1/main.cpp
@@ -31,18 +31,44 @@ int binarySearch(vector<int> entries, int target, int lower, int upper) {
     return lower;
 }
 
-// Returns the product of the two numbers that sum to the input
+// Returns the product of the two numbers that make up sum
 // Returns -1 if there are no such numbers
-int findComplements(vector<int> entries, int sum) {
+int find2Complements(vector<int> entries, int sum, int lower, int upper) {
     int target;
-    int upper = entries.size() - 1;
-    for (int i = 0; i < entries.size(); i++) {
+    for (int i = lower; i < upper; i++) {
         target = sum - entries[i];
         upper = binarySearch(entries, target, i + 1, upper);
         if (upper == -1) {
             return -1;
         } else if (entries[upper] == target) {
             return entries[i] * target;
+        }
+    }
+    return -1;
+}
+
+// Returns the product of the #count numbers that make up sum
+// Returns -1 if there is no solution
+// Only searches from [lower] upwards
+int findNComplements(vector<int> entries, int sum, int count, int lower, int upper) {
+    int result;
+    if (count == 2) {
+        return find2Complements(entries, sum, lower, upper);
+    }
+
+    // int lowestValues = 0;
+    // for (int i = lower; i < lower + count - 1; i++) {
+    //     lowestValues += entries[i];
+    // }
+    // result = binarySearch(entries, sum - lowestValues, lower + count, upper);
+    // if (result != -1) {
+    //     upper = result;
+    // }
+
+    for (int i = lower; i < entries.size(); i++) {
+        result = findNComplements(entries, sum - entries[i], count - 1, i + 1, upper);
+        if (result != -1) {
+            return entries[i] * result;
         }
     }
     return -1;
@@ -55,8 +81,6 @@ int main() {
 
     sort(entries.begin(), entries.end());
 
-    int result = findComplements(entries, 2020);
-
-    printf("%d\n", result);
-
+    printf("%d\n", findNComplements(entries, 2020, 2, 0, entries.size() - 1));
+    printf("%d\n", findNComplements(entries, 2020, 3, 0, entries.size() - 1));
 }
