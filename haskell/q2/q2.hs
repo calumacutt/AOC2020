fcfb8803aeaa909b455ed1823fdc14d15b839639
calumacutt <calumacutt@gmail.com>
2021-06-26 14:39:12 +1000

    q2 part b haskell

--- haskell/q2/q2.hs
+++ haskell/q2/q2.hs
@@ -22,16 +22,24 @@ convStringsToRange (a : b : []) = (read a, read b)
 
 --
 
-checkIfPolicyValid :: Policy -> Bool
-checkIfPolicyValid (Policy lower upper letter password) = if ((countLetterInPassword letter password)>=lower)&&((countLetterInPassword letter password)<=upper)
+checkIfPolicyValid1 :: Policy -> Bool
+checkIfPolicyValid1 (Policy lower upper letter password) = if ((countLetterInPassword letter password)>=lower)&&((countLetterInPassword letter password)<=upper)
     then True
     else False
 
 countLetterInPassword :: Char -> String -> Int
 countLetterInPassword letter password = foldr (\x y -> if (x == letter) then y + 1 else y) 0 password
 
+--
+
+checkIfPolicyValid2 :: Policy -> Bool
+checkIfPolicyValid2 (Policy lower upper letter password) = if ((password!!(lower-1) == letter) /= (password!!(upper-1) == letter))
+    then True
+    else False
+
 main :: IO ()
 main = do
     listOfStrings <- lines <$> readFile "input.txt"
     -- print(checkIfPolicyValid (convLineToPolicy (head listOfStrings)))
-    print(foldr (\x y -> if checkIfPolicyValid (convLineToPolicy x) then y + 1 else y) 0 listOfStrings)
+    -- print(foldr (\x y -> if checkIfPolicyValid1 (convLineToPolicy x) then y + 1 else y) 0 listOfStrings)
+    print(foldr (\x y -> if checkIfPolicyValid2 (convLineToPolicy x) then y + 1 else y) 0 listOfStrings)
